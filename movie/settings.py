0e2bae55a93f3cd891e98a3f7a19e870d47995ad
003 sis <ehrrh222@gmail.com>
2020-07-13 11:48:05 +0900

    project commit

--- movie/settings.py
+++ movie/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'movie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'movieTable',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': '',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
